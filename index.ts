d2ffe22ca0773354b48effca6ab43ee443c68201
laibay <laibaali858452@gmail.com>
2024-03-21 11:40:05 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+
+import inquirer from "inquirer";
+
+// console.log(answer);
+async function calculator() {
+  const answer = await inquirer.prompt([
+    { message: "ENTER FIRST NUMBER", type: "number", name: "firstnumber" },
+    { message: "ENTER SECOND NUMBER", type: "number", name: "secondnumber" },
+    {
+      message: "SELECT ONE OF THE OPERATORS TO PERFORM OPERATION",
+      type: "list",
+      name: "operator",
+      choices: ["Addition", "subtraction", "Multiplication", "Division"],
+    },
+  ]);
+  if (answer.operator === "Addition") {
+    console.log( answer.firstnumber + answer.secondnumber);
+  } else if (answer.operator === "subtraction") {
+    console.log( answer.firstnumber - answer.secondnumber);
+  } else if (answer.operator === "Multiplication") {
+    console.log( answer.firstnumber * answer.secondnumber);
+  } else if (answer.operator === "Division") {
+    console.log( answer.firstnumber / answer.secondnumber);
+  } else {
+    console.log( "ERROR");
+  }
+
+  const nextAction = await inquirer.prompt([{
+    message: "select an option",
+    type: "list",
+    choices: ["Recalculate", "Quit"],
+    name: "action"
+  }])
+if(nextAction.action === "Recalculate"){
+    calculator()
+} else {
+    return null
+}
+
+}
+calculator();
